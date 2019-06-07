f84ea15cc1d9085f2f7ab7048efba5a72f2d219b
Roger-luo <hiroger@qq.com>
2019-06-07 12:32:50 -0400

    export BatchedRoutines

--- src/CuBatchedRoutines.jl
+++ src/CuBatchedRoutines.jl
@@ -1,6 +1,8 @@
 module CuBatchedRoutines
 
 using GPUArrays, CuArrays
+using Reexport, BatchedRoutines
+@reexport BatchedRoutines
 import BatchedRoutines: batched_gemm!, batched_gemm, batched_tr!, batched_tr
 
 include("blas.jl")
